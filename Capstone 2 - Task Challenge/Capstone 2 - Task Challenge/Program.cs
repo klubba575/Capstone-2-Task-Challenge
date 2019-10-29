409ff065f65c167664b4ddb289c6699e77bca402
klubba575 <wilderandrew9@gmail.com>
2019-10-28 21:38:25 -0400

    2nd Capstone for GC Task Challenge

---
+++ Capstone 2 - Task Challenge/Capstone 2 - Task Challenge/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Capstone_2___Task_Challenge
+{
+	class Program
+	{
+		static bool goAgain = true;
+		static void Main(string[] args)
+		{  
+			while (goAgain)
+			{
+				PrintOptions();
+
+				string userInput = GetUserInput("Choose one of the 5 options: ");
+				
+				SelectMenuOption(userInput);
+			} 
+			Console.WriteLine("Have a great day!");
+		}
+
+		public static void SelectMenuOption(string userInput)
+		{
+			switch (userInput)
+			{
+				case "1":
+				case "list tasks":
+					Task.DisplayTasks();
+					break;
+				case "2":
+				case "Add Task":
+					CreateTask();
+					//Add Task Method
+					break;
+				case "3":
+				case "Delete task":
+					RemoveTask();
+					//Delete Task Method
+					break;
+				case "4":
+				case "mark task complete":
+					WhichTask();
+					//Mark Task Complete
+					break;
+				case "5":
+				case "Quit":
+					goAgain = false;
+					break;
+				default:
+					SelectMenuOption("That is not an option, please select again.");
+					break;
+			}
+		}
+		public static string GetUserInput(string message)
+		{
+			Console.WriteLine(message);
+			return Console.ReadLine();
+		}
+		public static void PrintOptions()
+		{
+			Console.WriteLine("1. List Tasks");
+			Console.WriteLine("2. Add Task");
+			Console.WriteLine("3. Delete Task");
+			Console.WriteLine("4. Mark task complete ");
+			Console.WriteLine("5. Quit");
+		}
+		public static void CreateTask()
+		{
+			Task task = new Task();
+			task.Name = GetUserInput("Enter Name: ");
+			task.Description = GetUserInput("Enter Description: ");
+			task.DueDate = (DateTime.Parse(GetUserInput("Enter a Date: ")));
+			task.Complete = false;
+
+			Task.AddTask(task);
+		}
+
+		public static DateTime ValiDate(string message)
+		{
+			try
+			{
+				return DateTime.Parse(GetUserInput(message));
+			}
+			catch (FormatException)
+			{
+				ValiDate("That won't work, we need a valid date!");
+			}
+			return ValiDate(message);
+		}
+		private static void RemoveTask()
+		{
+			Task.DisplayTasks();
+
+			string userInput = GetUserInput("Enter Task Number to Remove");
+
+			Task.DeleteTask(userInput);
+		}
+		public static void WhichTask()
+		{
+			Task.DisplayTasks();
+
+			string userInput = GetUserInput("Enter Task Number to Complete");
+
+			Task.CompleteTask(userInput);
+
+		}
+	}
+}
---
+++ Capstone 2 - Task Challenge/Capstone 2 - Task Challenge/Task.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone_2___Task_Challenge
+{
+	class Task
+	{
+		private static List<Task> _tasks = new List<Task>()
+		{
+			new Task("Bart Simpson", "Skateboards and wreaks havoc!", DateTime.Parse("10/25/2019")),
+			new Task("Homer Simpson", "Eats Donuts", DateTime.Parse("10/24/2019")),
+			new Task("Marge Simpson", "Keeps Everyone Out of Trouble", DateTime.Parse("10/26/2019")),
+			new Task("Lisa Simpson", "Plays a Mean Jazz Saxophone", DateTime.Parse("10/27/2019")),
+			new Task("Maggie Simpson", "Chews on the Pacifier", DateTime.Parse("10/28/2019")),
+		};
+		//fields
+		private string _name;
+		private string _description;
+		private DateTime _dueDate;
+		private bool _complete;
+
+		//properties
+		#region
+		public string Name 
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+		public string Description
+		{ 
+			 get { return _description; }
+			set { _description = value; }
+		}
+		public DateTime DueDate
+		{
+			get { return _dueDate;}
+			set { _dueDate = value; }
+		}
+		public bool Complete
+		{
+			get { return _complete; }
+			set { _complete = value; }
+		}
+		#endregion
+		//Constructors
+		#region
+		public Task() { }
+
+		public Task(string name, string description, DateTime dueDate, bool complete = false)
+		{
+			Name = name;
+			Description = description;
+			DueDate = dueDate;
+			Complete = complete;
+		}
+		#endregion
+
+		public static void PrintOptions()
+		{
+			Console.WriteLine("1. List Tasks");
+			Console.WriteLine("2. Add Task");
+			Console.WriteLine("3. Delete Task");
+			Console.WriteLine("4. Mark task complete ");
+			Console.WriteLine("5. Quit");
+		}
+		public static int ParseString(string message)
+		{
+			
+			int userChoice = int.Parse(message);
+			return userChoice;
+		}
+		public static void DisplayTasks()
+		{
+			for(int i = 0; i < _tasks.Count; i++)
+			{
+				Console.WriteLine($"\t{i+1}: {_tasks[i].Name}");
+				Console.WriteLine($"\tDue Date:{_tasks[i].DueDate.ToShortDateString()}");
+				Console.WriteLine($"\tComplete:{_tasks[i].Complete}");
+				Console.WriteLine($"\tDescription:{_tasks[i].Description}");
+			}
+		}
+		
+		public static void AddTask(Task task)
+		{
+			_tasks.Add(task);
+		}
+		public static void DeleteTask(string userInput)
+		{
+			int newNumber = ParseString(userInput);
+			_tasks.RemoveAt(newNumber - 1);
+
+		}
+		public static void CompleteTask(string userInput)
+		{
+			int number = int.Parse(userInput);
+			Task foundTask = _tasks[number-1];
+			foundTask.Complete = true;
+		}
+	}
+}
